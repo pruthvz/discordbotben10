d95589ef69e96b2052a93e2d3fe0d18d79af08fd
pruthvi10 <pruthvimohanlal10@gmail.com>
2021-10-04 12:03:16 +0100

    added images/new comamnds

--- ten.py
+++ ten.py
@@ -1,15 +1,39 @@
 from itertools import cycle
 import discord
 import random
+import requests
 from discord.ext import commands, tasks 
-from itertools import cycle 
+from itertools import cycle
+import os
+import json
 
 
 client = commands.Bot(command_prefix=".")
+STATUS_TIME = 2000
+
 
 aliens = ["Cannonbolt", "Overflow", "Heatblast", "XLR8", "Four Arms", "Grey Matter", "Diamondhead", "Upgrade", "Stinkfly", "Wildvine", "Gax", "Shock Rock", "Slapback", "Humungousaur", "Rath", "Jetray", "Goop", "Way Big", "Surge", "Spidermonkey", "Buzzshock", "Big Chill", "Chromastone", "Ampfibian", "Ripjaws", "Wildmutt", "Upchuck", "Ditto", "Eye Guy", "Ghostfreak", "Brainstorm", "Echo Echo", "Lodestar", "JuryRigg", "The Worst", "Feedback", "Gravattack", "Walkatrout", "NRG"]
+randomWords = ["soooxy woooonkie on the boonkie", "AWGOOOOOOOOOOO GOOOOOOOAAA BOOOOOOOOOO", "booonkie woonkie", "soooooxys 4 lite", "big ZOINKSYER HEHE", "mooooooooooooooooooooooooooooooonkay", "the soooooxi squid", "HAHSKDHASJHDAKJSHDUWADJAHSDKJASDJKAHDJASHDJASHDJA - splitzzy"]
 status = cycle(["big zoinks.", "slap grandpa max", "goms"])
-STATUS_TIME = 2000
+soosImage = "https://tenor.com/view/boiled-soundcloud-boiled-boiled-irl-boiled-utsc-boiled-cheesestick-agem-soundcloud-gif-20049996"
+# api
+
+def generateMeme():
+    global response
+    global preview
+    response = requests.get("https://meme-api.herokuapp.com/gimme")
+    preview = response.json()['preview'][2]
+
+generateMeme()
+
+def genMit():
+    path = r"./images/"
+    global random_filename
+    random_filename = random.choice([
+        x for x in os.listdir(path)
+        if os.path.isfile(os.path.join(path, x))
+    ])
+
 
 def generateAlienName(aliens):
     pickAlien = random.choice(aliens)
@@ -42,6 +66,10 @@ async def gen_error(ctx,error):
 async def clear(ctx, amount=5):
     await ctx.channel.purge(limit=amount)
 
+@client.command()
+async def cls(ctx, amount=5):
+    await ctx.channel.purge(limit=amount)
+
 # kick player
 @client.command()
 async def kick(ctx, member : discord.Member, *, reason="None"):
@@ -72,7 +100,7 @@ async def unban(ctx, *, member):
 @client.event 
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
-        await ctx.sends("bad command zoinks. hehee")
+        await ctx.send("bad command zoinks. hehee")
 
 @client.command()
 async def ping(ctx):
@@ -105,5 +133,28 @@ async def unmute(ctx, member: discord.Member):
     await member.remove_roles(mutedRole)
     await ctx.send(f"unzipped ur doonkie mooouth, {member.mention}")
     await member.send(f"you got unzoinked in the server, come speak again >> {ctx.guild.name}")
+
+# random iamges of mit
+@client.command()
+@commands.has_permissions(kick_members=True)
+async def mit(ctx):
+    genMit()
+    await ctx.send(file=discord.File(f"./images/{random_filename}"))
+    randomOutput = random.choice(randomWords)
+    await ctx.send(randomOutput)
+
+@client.command()
+async def meme(ctx):
+    generateMeme()
+    await ctx.send(preview)
+
+
+@client.command()
+async def joke(ctx):
+    await ctx.send(f"your a joke... ")
+
+@client.command()
+async def soos(ctx):
+    await ctx.send(soosImage)
 
 client.run("ODkzMDkxNDMzMjY1ODkzNDU2.YVWaNg.ifrgySRChu2qj5ncc3keKLMpL70")
